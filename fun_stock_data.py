3f1cc2d5814551118992394e7fdce18406ccda2c
Koplányi Dávid <55060571+KoplanyiDavid@users.noreply.github.com>
2024-10-07 00:20:46 +0200

    add difference check for 5% or greater movement

--- fun_stock_data.py
+++ fun_stock_data.py
@@ -1,9 +1,14 @@
 import requests
 from constants import ALPHA_VANTAGE_API_URL
 
-def get_stock_data() -> float:
+def get_stock_data(number_of_days_before: int) -> float:
+
+    if number_of_days_before < 0 or number_of_days_before > 20:
+        print("Number of days before must be between 0 and 20!")
+        raise ValueError
+    
     response = requests.get(ALPHA_VANTAGE_API_URL)
     response.raise_for_status()
     data = response.json()["Time Series (Daily)"]
     data_list = [value for (key,value) in data.items()]
-    return(data_list[0]["4. close"])
+    return(data_list[number_of_days_before]["4. close"])
--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from fun_news_data import get_news_data
 from fun_stock_data import get_stock_data
 from fun_messaging import send_message
 
+# Check stock price movement within 2 days
+last_day_price = get_stock_data(number_of_days_before=0)
+day_before_last_day_price = get_stock_data(number_of_days_before=1)
+difference_in_percentage = (abs(1 - (last_day_price / day_before_last_day_price))) * 100
 
 message = ""
 
-# send the message at 8am in the morning
-if 8 == datetime.now().hour:
+# send the message at 8am in the morning if the movement is bigger than 5%
+if 8 == datetime.now().hour and difference_in_percentage > 5:
 
     # construct the message
-    message += f"SEDG closing price: {get_stock_data()}"
+    message += f"SEDG closing price: {last_day_price}"
     for news in get_news_data():
         message += "\n"
         message += str(news)
